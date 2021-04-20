e4f9fded308b6fea74037920dda54c9447dbc4e6
JarrettV <jarrettv@gmail.com>
2021-04-19 20:50:36 -0500

    fix hpma serial read of packets

--- hpma115c0.h
+++ hpma115c0.h
@@ -11,48 +11,75 @@ class Hpma115C0Component : public PollingComponent, public UARTDevice {
   Sensor *pm_4_sensor = new Sensor();
   Sensor *pm_10_sensor = new Sensor();
 
+  static const uint16_t STARTUP_DELAY_MS = 20000;
+
+  bool delaying_startup = true;
+  
   static const char MSG_CHAR_1 = 0x42;
   static const char MSG_CHAR_2 = 0x4d;
-
-  int sum = 0;
-  uint8_t count = 0;
-  unsigned char data[32] = {};
-
+  bool packet_found = false;
+  std::array<u_char,32> last_good_packet = {};
+  
   void setup() override {
-    // nothing to do here
+    this->set_timeout("startup_delay", STARTUP_DELAY_MS, [this]() {
+      ESP_LOGI("hpma115c0", "Startup delay completed");
+      this->delaying_startup = false;
+    });
   }
+
   void update() override {
 
-    sum = 0;
-    count = 0;
-    data[32] = {};
-
-    while (available() && count < 32) {
-      data[count] = read();
-      if (count == 0 && data[0] != MSG_CHAR_1) {
-        ESP_LOGD("hpma115c0", "Unexpected first char %c", data[0]);
-        break;
-      }
-      if (count == 1 && data[1] != MSG_CHAR_2) {
-        ESP_LOGD("hpma115c0", "Unexpected second char %c", data[1]);
-        break;
-      }
-      if (count < 30) sum += data[count];
-      count++;
-    }
-
-    if (count < 32 || sum == 0) {
-      ESP_LOGW("hpma115c0", "Unable to read packet");
+    if (delaying_startup) {
+      ESP_LOGI("hpma115c0", "Startup delay in progress");
       return;
     }
 
-    int checksum = (data[30] << 8) | data[31];
+    read_packets();
+
+    publish_state(last_good_packet);
+  }
+
+  void read_packets() {
+    int count = 0;
+    std::array<u_char,32> data = {};
+
+    // only start counting up when the first 2 characters are found
+    while (available()) {
+      data[count] = read();
+      
+      if ((data[0] == MSG_CHAR_1 && count == 0) || data[1] == MSG_CHAR_2) count++;
+      else read_packets();
+
+      // full packet
+      if (count == 32) {
+        handle_packet(data);
+        read_packets();
+      }
+    }
+  }
+
+  void handle_packet(std::array<u_char,32> data) {
+
+    ESP_LOGD("hpma115c0", "handle_packet");
+
+    uint8_t sum = 0;
+    for (int i = 0; i < 30; i++) sum += data[i];
+    uint8_t checksum = (data[30] << 8) | data[31];
 
     if (sum != checksum) {
       ESP_LOGW("hpma115c0", "Checksum invalid: %i <> %i", sum, checksum);
       return;
     }
 
+    last_good_packet = data;
+    packet_found = true;
+  }
+
+  void publish_state(const std::array<u_char,32> &data) {
+
+    if (!packet_found) return;
+
+    // read data
     auto pm = (data[4] * 256) + data[5];
     pm_1_sensor->publish_state(pm);
 
